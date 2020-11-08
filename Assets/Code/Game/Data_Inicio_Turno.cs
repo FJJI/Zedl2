67c05a63cd51ad16f7aa8220fabd89b4aa7bf5ce
FJJI <fjjimenez@miuandes.cl>
2020-11-07 23:08:01 -0300

    inicio escena Game

---
+++ Assets/Code/Game/Data_Inicio_Turno.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data_Inicio_Turno : MonoBehaviour
+{
+    static public Data_Inicio_Turno data;
+    public int matchID;  // para sacar - poner la data
+    public int turn;
+    public int playerTurn;  // para saber a quien le damos el beneficio de jugar, sino poner una nota
+    public int InitialPlayers;  // para saber la cantidad de la player para armar la partida, los cambios de turno y saber cuando alguien pierde
+    public List<bool> defeated;
+
+    // Los Nodos + la flecha 
+    public GameObject Normal;
+    public GameObject Extra;
+    public GameObject Ataque;
+    public GameObject Defensa;
+    public GameObject Flecha;
+
+
+    void Awake()  // Hacemos que la esta data exista en lapartida y de ser necesario, desde el room para su trata con la informacion
+    {
+        if (data == null)
+        {
+            data = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (data != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Code/Game/Inicio_Ronda.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inicio_Ronda : MonoBehaviour
+{
+    Data_Inicio_Turno data;
+
+    void setup()
+    {
+        if (data.turn == 0) // si es el primer turno (turno 0 para el armado), armo una nueva partida, de lo contrario, cargo los datos existentes.
+        {
+            defaultStart();
+        }
+        else  // Cargo la partida guardada
+        {
+
+        }
+    }
+
+    void defaultStart()
+    {
+        GameObject Normal = data.Normal;
+        GameObject Extra = data.Extra;
+        GameObject Ataque = data.Ataque;
+        GameObject Defensa = data.Defensa;
+
+        GameObject e = Instantiate(Normal, new Vector3(7, 3, -1), Quaternion.identity);
+        GameObject n = Instantiate(Extra, new Vector3(7, -3, -1), Quaternion.identity);
+        GameObject a = Instantiate(Ataque, new Vector3(-7, 3, -1), Quaternion.identity);
+        GameObject d = Instantiate(Defensa, new Vector3(-7, -3, -1), Quaternion.identity);
+        GameObject n2 = Instantiate(Normal, new Vector3(4, -2, -1), Quaternion.identity);
+        GameObject n3 = Instantiate(Normal, new Vector3(4, 2, -1), Quaternion.identity);
+        GameObject n4 = Instantiate(Normal, new Vector3(-4, -2, -1), Quaternion.identity);
+        GameObject n5 = Instantiate(Normal, new Vector3(-4, 2, -1), Quaternion.identity);
+        GameObject n6 = Instantiate(Normal, new Vector3(0, 3, -1), Quaternion.identity);
+        GameObject n7 = Instantiate(Normal, new Vector3(0, -3, -1), Quaternion.identity);
+
+    }
+
+    void Start()
+    {
+        data = GameObject.Find("DataAGuardar").GetComponent<Data_Inicio_Turno>();
+        setup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
